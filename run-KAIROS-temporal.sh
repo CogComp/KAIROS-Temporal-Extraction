753974c7ae3f32ed8f403ed8252f3eeed491080d
Yuqian Deng <68020340+Michael-Dyq@users.noreply.github.com>
2021-08-08 21:29:50 -0400

    Update run-KAIROS-temporal.sh(gurobi_client.lic)

--- run-KAIROS-temporal.sh
+++ run-KAIROS-temporal.sh
@@ -1,6 +1,7 @@
 # export NVIDIA_VISIBLE_DEVICES=-1
 #export GRB_LICENSE_FILE=/shared/ccgadmin/demos/KAIROS2020-demo/events/gurobi.lic
-export GRB_LICENSE_FILE=/shared/ccgadmin/demos/KAIROS2020-demo/events/gurobi_client_server.lic
+#export GRB_LICENSE_FILE=/shared/ccgadmin/demos/KAIROS2020-demo/events/gurobi_client_server.lic
+export GRB_LICENSE_FILE=/shared/ccgadmin/app/gurobi/gurobi_client.lic
 export CUDA_VISIBLE_DEVICES=-1;
 while [ 1 -eq 1 ]
 do
